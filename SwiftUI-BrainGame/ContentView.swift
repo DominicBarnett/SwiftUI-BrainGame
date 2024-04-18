94b2bd91ba8812688a0c4c072fd4dca21e88a507
DominicBarnett <dominic.barnett@icloud.com>
2024-04-18 13:51:00 -0700

    Initial Commit

---
+++ SwiftUI-BrainGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-BrainGame
+//
+//  Created by Dominic Barnett on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-BrainGame/SwiftUI_BrainGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BrainGameApp.swift
+//  SwiftUI-BrainGame
+//
+//  Created by Dominic Barnett on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BrainGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
